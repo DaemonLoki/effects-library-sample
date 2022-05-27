5bc64f6a9ad17bec43038bc203f43db6d7c83ac0
Stefan <stefan.blos@gmail.com>
2022-05-27 14:39:14 +0200

    Initial Commit

---
+++ effectslibrary-sample/effectslibrary_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  effectslibrary_sampleApp.swift
+//  effectslibrary-sample
+//
+//  Created by Stefan Blos on 27.05.22.
+//
+
+import SwiftUI
+
+@main
+struct effectslibrary_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

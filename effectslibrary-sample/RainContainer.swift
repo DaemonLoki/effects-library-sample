0182fd9d069a3a2cb2dd4b5d1faa1788bf46b27b
Stefan <stefan.blos@gmail.com>
2022-05-30 18:11:56 +0200

    Rain effect samples added

--- effectslibrary-sample/RainContainer.swift
+++ effectslibrary-sample/RainContainer.swift
@@ -1,5 +1,5 @@
 //
-//  SmokeContainer.swift
+//  RainContainer.swift
 //  effectslibrary-sample
 //
 //  Created by Stefan Blos on 30.05.22.
@@ -8,16 +8,16 @@
 import SwiftUI
 import EffectsLibrary
 
-enum SmokeExample: String, CaseIterable {
-    case regular, intense, subtle, branded
+enum RainExample: String, CaseIterable {
+    case regular, intense, subtle, branded, reversed
     
     @ViewBuilder func view() -> some View {
         switch self {
         case .regular:
-            SmokeView()
+            RainView()
         case .intense:
-            SmokeView(
-                config: SmokeConfig(
+            RainView(
+                config: RainConfig(
                     intensity: .high,
                     lifetime: .long,
                     initialVelocity: .fast,
@@ -25,8 +25,8 @@ enum SmokeExample: String, CaseIterable {
                 )
             )
         case .subtle:
-            SmokeView(
-                config: SmokeConfig(
+            RainView(
+                config: RainConfig(
                     intensity: .low,
                     lifetime: .short,
                     initialVelocity: .slow,
@@ -34,13 +34,13 @@ enum SmokeExample: String, CaseIterable {
                 )
             )
         case .branded:
-            SmokeView(
-                config: SmokeConfig(
+            RainView(
+                config: RainConfig(
                     content: [
                         .image(UIImage(named: "stream-logo-blue")!, UIColor(Color.blue.opacity(0.4)), 0.05),
                         .image(UIImage(named: "stream-logo-white")!, UIColor(Color.white.opacity(0.4)), 0.05),
                         .image(UIImage(named: "stream-logo-black")!, UIColor(Color.black.opacity(0.4)), 0.05),
-                        .emoji("🔥", 1)
+                        .emoji("💧", 0.2)
                     ],
                     backgroundColor: .blue.opacity(0.3),
                     intensity: .low,
@@ -50,20 +50,33 @@ enum SmokeExample: String, CaseIterable {
                     spreadRadius: .high
                 )
             )
+        case .reversed:
+            RainView(
+                config: RainConfig(
+                    intensity: .high,
+                    lifetime: .long,
+                    initialVelocity: .fast,
+                    fadeOut: .none,
+                    emitterPosition: .bottom,
+                    fallDirection: .upwards
+                )
+            )
         }
+    
+        
     }
 }
 
-struct SmokeContainer: View {
+struct RainContainer: View {
     
-    @State private var currentExample: SmokeExample = .regular
+    @State private var currentExample: RainExample = .regular
     
     var body: some View {
         ZStack {
             currentExample.view()
             
             ScrollView {
-                ForEach(SmokeExample.allCases, id: \.self) { example in
+                ForEach(RainExample.allCases, id: \.self) { example in
                     Button {
                         currentExample = example
                     } label: {
@@ -84,9 +97,8 @@ struct SmokeContainer: View {
         .navigationTitle("Fireworks examples")
     }
 }
-
-struct SmokeContainer_Previews: PreviewProvider {
+struct RainContainer_Previews: PreviewProvider {
     static var previews: some View {
-        SmokeContainer()
+        RainContainer()
     }
 }
